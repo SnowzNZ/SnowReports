84b772bf5852a25309b91bed36cdee232ad84f5e
SnowzNZ <jackson.baylis@outlook.co.nz>
2023-06-18 20:00:47 +1200

    init

---
+++ src/main/java/me/snowznz/snowreports/SnowReports.java
@@ -0,0 +1,31 @@
+package me.snowznz.snowreports;
+
+import me.snowznz.snowreports.commands.Report;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public class SnowReports extends JavaPlugin {
+    FileConfiguration config = this.getConfig();
+
+    @Override
+    public void onEnable() {
+
+        config.addDefault("discord-webhook-url", "");
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        if (Objects.equals(config.getString("discord-webhook-url"), "")) {
+            getLogger().warning("discord-webhook-url is not set, reports wont be sent to discord!");
+        }
+
+        Objects.requireNonNull(getCommand("report")).setExecutor(new Report(config));
+
+
+        Permission receiveReport = new Permission("snowreports.report.receive");
+        getServer().getPluginManager().addPermission(receiveReport);
+
+    }
+}
---
+++ src/main/java/me/snowznz/snowreports/commands/Report.java
@@ -0,0 +1,82 @@
+package me.snowznz.snowreports.commands;
+
+import me.snowznz.snowreports.utils.DiscordWebhook;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.bukkit.Bukkit.getOnlinePlayers;
+import static org.bukkit.Bukkit.getPlayer;
+
+public class Report implements CommandExecutor {
+    private final FileConfiguration config;
+
+    public Report(FileConfiguration config) {
+        this.config = config;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player reporter)) {
+            sender.sendMessage("Only players can run this command!");
+            return true;
+        }
+
+        if (args.length < 2) {
+            sender.sendMessage("Usage: /report <player> <reason>");
+            return true;
+        }
+
+        Player reportedPlayer = getPlayer(args[0]);
+        if (reportedPlayer == null) {
+            sender.sendMessage("Player not found!");
+            return true;
+        }
+
+        String reason = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+
+        if (reporter.hasPermission("snowreports.report")) {
+            if (!(Objects.equals(config.getString("discord-webhook-url"), ""))) {
+                DiscordWebhook webhook = new DiscordWebhook(config.getString("discord-webhook-url"));
+                webhook.addEmbed(new DiscordWebhook.EmbedObject()
+                        .setDescription("**" + reportedPlayer.getName() + "** has been reported for: " + reason)
+                        .setFooter("Reported by: " + reporter.getName(), "https://mc-heads.net/avatar/" + reporter.getUniqueId()));
+                webhook.setUsername("SnowReports");
+
+                try {
+                    webhook.execute();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            reporter.sendMessage(ChatColor.GREEN + "Your report has been sent!");
+
+            for (Player admin : getOnlinePlayers()) {
+                if (admin.hasPermission("snowreports.report.receive")) {
+                    TextComponent message = new TextComponent(ChatColor.RED + reportedPlayer.getName() + " has been reported for: " + reason);
+                    message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Reported by " + reporter.getName() + ". Click to teleport to " + reportedPlayer.getName())));
+                    message.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/tp " + reportedPlayer.getName()));
+                    admin.spigot().sendMessage(message);
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
