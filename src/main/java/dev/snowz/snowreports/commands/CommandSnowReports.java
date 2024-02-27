69cf01a78f50c1871491eae34a23e807fd18bb2e
SnowzNZ <98007575+SnowzNZ@users.noreply.github.com>
2024-02-27 21:48:26 +1300

    add enable and disable args to snowreports command

--- src/main/java/dev/snowz/snowreports/commands/CommandSnowReports.java
+++ src/main/java/dev/snowz/snowreports/commands/CommandSnowReports.java
@@ -6,22 +6,42 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class CommandSnowReports implements CommandExecutor, TabCompleter {
 
+
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        SnowReports.getInstance().reloadConfig();
-        sender.sendMessage("§aReloaded config!");
-        return true;
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 0) {
+            return false;
+        }
+
+        if (args[0].equalsIgnoreCase("enable")) {
+            SnowReports.getInstance().getConfig().set("reports.enabled", true);
+            SnowReports.getInstance().saveConfig();
+            SnowReports.getInstance().reloadConfig();
+            sender.sendMessage("§aEnabled reports!");
+            return true;
+        } else if (args[0].equalsIgnoreCase("disable")) {
+            SnowReports.getInstance().getConfig().set("reports.enabled", false);
+            SnowReports.getInstance().saveConfig();
+            SnowReports.getInstance().reloadConfig();
+            sender.sendMessage("§cDisabled reports!");
+            return true;
+        } else if (args[0].equalsIgnoreCase("reload")) {
+            SnowReports.getInstance().reloadConfig();
+            sender.sendMessage("§aReloaded config!");
+            return true;
+        }
+        return false;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (args.length == 1) {
-            return Collections.singletonList("reload");
+            return Arrays.asList("enable", "disable", "reload");
         }
         return null;
     }
