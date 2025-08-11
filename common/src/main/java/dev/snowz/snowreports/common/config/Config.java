8c45e44225bcc2287f9a136e5ad57b169f5e4602
SnowzNZ <98007575+SnowzNZ@users.noreply.github.com>
2025-08-12 11:30:41 +1200

    chore: update config comments

--- common/src/main/java/dev/snowz/snowreports/common/config/Config.java
+++ common/src/main/java/dev/snowz/snowreports/common/config/Config.java
@@ -41,20 +41,18 @@ public final class Config {
 
     private boolean metrics = true;
 
-    @Comment(
-        """
-               - Possible options:
-            
-                 |  Remote databases - require connection information to be configured below
-                 |=> MySQL
-                 |=> MariaDB
-                 |=> PostgreSQL
-            
-                 |  Flatfile/local database - don't require any extra configuration
-                 |=> H2
-                 |=> SQLite
-            """
-    )
+    @Comment({
+        "Possible options:",
+        "",
+        "Remote databases - requires connection information to be configured below",
+        "- MySQL",
+        "- MariaDB",
+        "- PostgreSQL",
+        "",
+        "Flatfile/local database - no extra configuration required",
+        "- SQLite (default)",
+        "- H2"
+    })
     private DatabaseType storageMethod = DatabaseType.SQLITE;
 
     private DatabaseConfig database = new DatabaseConfig();
@@ -85,6 +83,7 @@ public final class Config {
     @Getter
     @Setter
     @Configuration
+    @NoArgsConstructor
     public static class DatabaseConfig {
         private String host = "localhost";
         private int port = 3306;
@@ -93,13 +92,12 @@ public final class Config {
         private String username = "username";
         private String password = "password";
 
-        @Comment(
-            """
-                   These settings apply to the MySQL connection pool.
-                   - The default values will be suitable for the majority of users.
-                   - Do not change these settings unless you know what you're doing!
-                """
-        )
+        @Comment({
+            "These settings apply to the remote database connection pool.",
+            "",
+            "- The default values will be suitable for the majority of users.",
+            "- Do not change these settings unless you know what you're doing!"
+        })
         private PoolSettingsConfig poolSettings = new PoolSettingsConfig();
 
         private AdvancedConfig advanced = new AdvancedConfig();
@@ -107,6 +105,7 @@ public final class Config {
         @Getter
         @Setter
         @Configuration
+        @NoArgsConstructor
         public static class PoolSettingsConfig {
             private int maximumPoolSize = 10;
             private int minimumIdle = 10;
@@ -118,15 +117,12 @@ public final class Config {
         @Getter
         @Setter
         @Configuration
+        @NoArgsConstructor
         public static class AdvancedConfig {
-            @Comment(
-                """
-                    The prefix for all SnowReports SQL tables.
-                    
-                    - This only applies for remote SQL storage types (MySQL, MariaDB, etc).
-                    - Change this if you want to use different tables for different servers.
-                    """
-            )
+            @Comment({
+                "- This only applies for remote storage methods (MySQL, MariaDB, PostgreSQL).",
+                "- Change this if you want to use the same database for multiple servers."
+            })
             private String tablePrefix = "snowreports_";
             private boolean useSsl = false;
             private boolean verifyServerCertificate = false;
@@ -137,8 +133,10 @@ public final class Config {
     @Getter
     @Setter
     @Configuration
+    @NoArgsConstructor
     public static class ReportsConfig {
         private boolean enabled = true;
+        @Comment("The cooldown in seconds before a player can report again. Set to -1 for no cooldown.")
         private int cooldown = 15;
         @Comment("The maximum number of reports a player can have Open or In Progress. Set to -1 for no limit.")
         private int playerLimit = -1;
@@ -149,6 +147,7 @@ public final class Config {
         @Getter
         @Setter
         @Configuration
+        @NoArgsConstructor
         public static class Reason {
             private boolean required = true;
             private String[] presets = { "Cheating", "Spamming", "Griefing", "Toxicity" };
@@ -157,9 +156,15 @@ public final class Config {
         @Getter
         @Setter
         @Configuration
+        @NoArgsConstructor
         public static class ChatHistory {
             private boolean enabled = true;
+            @Comment("How many messages to store in the report.")
             private int maxMessages = 5;
+            @Comment({
+                "How long to keep the chat history stored in memory.",
+                "If a report is created after this time, the message will not be included in the report's chat history."
+            })
             private int maxAgeSeconds = 1800;
         }
     }
@@ -167,6 +172,7 @@ public final class Config {
     @Getter
     @Setter
     @Configuration
+    @NoArgsConstructor
     public static class DiscordConfig {
         private boolean enabled = false;
         private String webhookUrl = "";
@@ -175,6 +181,7 @@ public final class Config {
         @Getter
         @Setter
         @Configuration
+        @NoArgsConstructor
         public static class Embed {
             private String title = "SnowReports";
             private String hexColor = "#03c2fc";
@@ -184,6 +191,7 @@ public final class Config {
     @Getter
     @Setter
     @Configuration
+    @NoArgsConstructor
     public static class DebugConfig {
         private boolean allowSelfReport = false;
     }
